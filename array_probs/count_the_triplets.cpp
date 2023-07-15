c3fbb791a97d5581396d2a5e91c06143973be8a7
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-07-15 16:42:48 +0530

    count_the_triplets

--- array_probs/count_the_triplets.cpp
+++ array_probs/count_the_triplets.cpp
@@ -5,7 +5,7 @@ public:
 	{
 	    int count = 0;
  
-        // Loop to count for triplets
+        // Loop to count for triplets - o(n^3) approach
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {   
                 for (int k = j + 1; k < n; k++) {
@@ -23,7 +23,7 @@ public:
         }
         return count;
     }
-// 	    // Your code goes here
+// 	    // Your code goes here - o(n^2) approach
 // 	    int i, sum;
 // 	    int cnt = 0;
 // 	    unordered_map<int, int> um;
@@ -41,3 +41,57 @@ public:
 // 	    }
 // 	}
 // 	return cnt;
+
+
+//  gfg solution 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+    // Function to count the number of ways
+    // to choose the triples
+    int countWays(int arr[], int n)
+    {
+        // compute the max value in the array
+        // and create frequency array of size
+        // max_val + 1.
+        // We can also use HashMap to store
+        // frequencies. We have used an array
+        // to keep remaining code simple.
+        int max_val = 0;
+        for (int i = 0; i < n; i++)
+            max_val = max(max_val, arr[i]);
+        int freq[max_val + 1]={0};
+        for (int i = 0; i < n; i++)
+            freq[arr[i]]++;
+ 
+        int ans = 0; // stores the number of ways
+ 
+        // Case 1: 0, 0, 0
+        ans += freq[0] * (freq[0] - 1) * (freq[0] - 2) / 6;
+ 
+        // Case 2: 0, x, x
+        for (int i = 1; i <= max_val; i++)
+            ans += freq[0] * freq[i] * (freq[i] - 1) / 2;
+ 
+        // Case 3: x, x, 2*x
+        for (int i = 1; 2 * i <= max_val; i++)
+            ans += freq[i] * (freq[i] - 1) / 2 * freq[2 * i];
+ 
+        // Case 4: x, y, x + y
+        // iterate through all pairs (x, y)
+        for (int i = 1; i <= max_val; i++) {
+            for (int j = i + 1; i + j <= max_val; j++)
+                ans += freq[i] * freq[j] * freq[i + j];
+        }
+ 
+        return ans;
+    }
+ 
+    // Driver code
+    int main()
+    {
+        int arr[]={ 1, 2, 3, 4, 5 };
+        int n = sizeof(arr)/sizeof(int);
+        cout<<(countWays(arr, n));
+        return 0;
+    }
