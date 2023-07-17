dcad0781459d51de361f0fe260fa4e72bec5e643
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-07-17 13:35:39 +0530

    sorting_array_of102

--- array_probs/sorting_array_of102.cpp
+++ array_probs/sorting_array_of102.cpp
@@ -50,4 +50,19 @@ void sort012(int a[], int n)
 
 //O(N) solution
         
-    }
+// int low = 0, mid = 0, high = nums.size()-1;
+//         while(mid <= high){
+//             if(nums[mid] == 0){
+//                 swap(nums[low], nums[mid]);
+//                 low++;
+//                 mid++;
+//             }
+//             else if(nums[mid] == 1){
+//                 mid++;
+//             }
+//             else{
+//                 swap(nums[mid], nums[high]);
+//                 high--;
+//             }
+//         }
+//     }
