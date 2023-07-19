728a6f96f63467d65bd166b8b80a4fecb7f8dd45
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-07-19 14:09:13 +0530

    rotate_matrix

--- array_11/rotate_matrix.cpp
+++ array_11/rotate_matrix.cpp
@@ -10,3 +10,28 @@ public:
         }
     }
 };
+
+
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+        
+//         int n= matrix.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 swap(matrix[i][j],matrix[j][i]);
+//             }//swaped the elements [i][j] to [j][i] toobtain the transpose of matrix
+//         }
+        
+//         //now reversing the rows in below code
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n/2;j++){
+//                 swap(matrix[i][j],matrix[i][n-1-j]);
+//             }
+//         }
+        
+//     }
+
+
+//O(N^2) SOLUTION
+//A BRUTE FORCE APPROACH
