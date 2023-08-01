1e39fba8b6cb42e851465c749be73fefd49e3fa2
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-08-01 20:17:57 +0530

    combinations

---
+++ top interview questions/math/combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1) result.push_back(p);
+			else {
+			    ++i;
+			    p[i] = p[i - 1];
+			}
+		}
+		return result;
+	}
+};
+
+
+// Input: n = 4, k = 2
+// Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+// Explanation: There are 4 choose 2 = 6 total combinations.
+// Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+
+
+//   Input: n = 1, k = 1
+// Output: [[1]]
+// Explanation: There is 1 choose 1 = 1 total combination.
