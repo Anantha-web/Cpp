22192c58d89c6705664efc5910c94a5663f2b142
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-08-05 11:05:47 +0530

    say_digit

---
+++ recursion/say_digit.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void sayDigit(int n, int arr[10]){
+    // base case
+    if(n ==0)
+        return 0;  
+    int digit = n % 10;
+    cout << "digit" << arr[];
+
+}
+
+int main(){
+  int n = 2;
+  int arr[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+  cout<<endl;
+
+  // calling the function
+  sayDigit(n, arr[10]);
+
+}
+  
