0075356a6829d5112135f68e83533de436286b70
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-08-12 22:44:09 +0530

    add_digits

--- array_11/add_digits.cpp
+++ array_11/add_digits.cpp
@@ -5,6 +5,22 @@ public:
     }
 };
 
+// class Solution {
+// public:
+//     int addDigits(int num) {
+//         while(true){
+//             string s1 = to_string(num);
+//             if(s1.length() == 1)break;
+//             num = 0;
+//             for(int i = 0;i < s1.length();i++){
+//                 num += s1[i] - '0';
+//             }
+
+//         }
+//         return num;
+//     }
+// };
+
 
 // Input: num = 38
 // Output: 2
