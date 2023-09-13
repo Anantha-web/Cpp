74586a9cde83a93f789c07a1305d9e7d297aa186
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-09-13 10:20:07 +0530

    candy

--- leetcode/candy.cpp
+++ leetcode/candy.cpp
@@ -26,3 +26,18 @@ public:
         return ans;
     }
 };
+
+
+// Input: ratings = [1,0,2]
+// Output: 5
+// Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+// Input: ratings = [1,2,2]
+// Output: 4
+// Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+// The third child gets 1 candy because it satisfies the above two conditions.
+
+// #Constraints
+// n == ratings.length
+// 1 <= n <= 2 * 104
+// 0 <= ratings[i] <= 2 * 104
