f650d43ecaeee18ff96f4d2a5018195abe94a424
Anantha Lakshmi <82811649+Anantha-web@users.noreply.github.com>
2023-09-19 11:00:55 +0530

    find_the_duplicate_in_array

--- array_11/find_the_duplicate_in_array.cpp
+++ array_11/find_the_duplicate_in_array.cpp
@@ -17,3 +17,18 @@ public:
         
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             int temp = abs(nums[i]);
+//             arr[temp-1] = -nums[temp-1];
+//             if(nums[temp-1] > 0) return temp;
+//         }     
+//         return -1;   
+//     }
+// };
